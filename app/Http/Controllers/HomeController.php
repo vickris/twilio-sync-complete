c0e04ef202669ec97c3962c0695e51befc4a35b1
vickris <christophervundi@gmail.com>
2019-02-13 22:54:16 +0300

    Install chart.js and lodash

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,35 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Twilio\Jwt\AccessToken;
-use Twilio\Jwt\Grants\SyncGrant;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    // This function should be held in a more secure class in your code
-    protected function getToken() {
-        // Create yourself an identity for your token
-        $identity = "SuperUser123";
-
-        // Creates an access token, which we will then serialize and send to the client
-        $token = new AccessToken(
-            env('TWILIO_ACCOUNT_SID'),
-            env('TWILIO_SYNC_API_KEY'),
-            env('TWILIO_SYNC_API_SECRET'),
-            3600,
-            $identity
-        );
-
-        // Grant access to Sync
-        $syncGrant = new SyncGrant();
-        $syncGrant->setServiceSid(env('TWILIO_SYNC_SERVICE_SID'));
-        $token->addGrant($syncGrant);
-
-        return $token->toJWT();
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
     }
 
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
     public function index()
     {
-        return view('welcome', ['token' => $this->getToken()]);
+        return view('home');
     }
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+<voting></voting>
+@endsection
